d96593bb674f2adaed308a0549b7f3f6758a56fd
Gourav Auluck <gore.auluck@gmail.com>
2022-09-14 09:51:00 -0700

    Check if String is Prefix of Array

---
+++ check-if-string-is-prefix-of-array/index.js
@@ -0,0 +1,24 @@
+// Directions
+
+// Given a string s and an array of strings words, determine whether s is a prefix string of words.
+
+// A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+
+// Return true if s is a prefix string of words, or false otherwise.
+
+function isPrefixString(s, words) {
+  // define result variable as empty string
+  let result = '';
+  // iterate through words array
+  for (let i = 0; i < s.length; i++) {
+    // increment result string by each word in the array
+    result += words[i];
+    // check to see if result equals value of s string
+    // if result equals s then return true
+    if (result === s) {
+      return true;
+    }
+  }
+  // return false
+  return false;
+}
