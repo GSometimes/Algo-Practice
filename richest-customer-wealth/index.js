3c519e8dc4fd2c200a4a007d141104bc67168728
Gore Sometimes <gore.auluck@gmail.com>
2022-11-07 23:59:34 -0800

    Richest Customer Wealth

---
+++ richest-customer-wealth/index.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/richest-customer-wealth/
+
+// Directions
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+// Solution # 1
+
+function maximumWealth(accounts) {
+    // define variable for greater wealth
+    // iterate through parent array
+        // define counter to find some of money
+}
