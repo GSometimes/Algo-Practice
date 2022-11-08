dd610633f49ffd82c0c7c7d3d8428dfca102b19e
Gore Sometimes <gore.auluck@gmail.com>
2022-11-08 00:09:40 -0800

    Richest Customer Wealth

--- richest-customer-wealth/index.js
+++ richest-customer-wealth/index.js
@@ -9,7 +9,47 @@
 // Solution # 1
 
 function maximumWealth(accounts) {
-    // define variable for greater wealth
-    // iterate through parent array
-        // define counter to find some of money
+  // define variable for greater wealth
+  let greaterWealth = 0;
+
+  // iterate through parent array
+  for (let i = 0; i < accounts.length; i++) {
+    // define counter for sum of each account
+    let currentWealth = 0;
+    // iterate through child array
+    // increment sum by each dollar amount in array
+    for (let j = 0; j < accounts[i].length; j++) {
+      currentWealth += accounts[i][j];
+    }
+    // set the value of wealth to the max value between wealth and sum
+    greaterWealth = Math.max(currentWealth, greaterWealth);
+  }
+  // return wealth
+  return greaterWealth;
 }
+
+// Solution # 2
+
+function maximumWealth(accounts) {
+  // define variable for greater wealth
+  let greaterWealth = 0;
+
+  // iterate through accounts
+  for (let i = 0; i < accounts.length; i++) {
+    // define variable that calculates the sum of each account with reducer
+    let currentWealth = accounts[i].reduce((prev, curr) => prev + curr);
+
+    // if current wealth is greater than greater wealth
+    // set value of greater wealth to that of current wealth
+    if (currentWealth > greaterWealth) {
+      greaterWealth = currentWealth;
+    }
+  }
+
+  // return greater wealth
+  return greaterWealth;
+}
+
+
+console.log(maximumWealth([[1,2,3],[3,2,1]])); // 6
+console.log(maximumWealth([[1,5],[7,3],[3,5]])); // 10
