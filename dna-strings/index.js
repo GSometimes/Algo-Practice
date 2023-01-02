5da130bad3c00079a887ef41eff9ce75cce77117
Gore Sometimes <gore.auluck@gmail.com>
2023-01-01 23:40:24 -0800

    Added similar solution with pairs object

--- dna-strings/index.js
+++ dna-strings/index.js
@@ -19,18 +19,58 @@
 
 let dnaString = 'ATTGC';
 
+// const dnaTransform = (string) => {
+//   // YOUR CODE GOES HERE
+//   let output = '';
+
+//   for (const el of string) {
+//     if (el === 'A') {
+//       output += 'T'
+//     } else if (el === 'T') {
+//       output += 'A'
+//     } else if (el === 'C') {
+//       output += 'G'
+//     } else if (el === 'G') {
+//       output += 'C'
+//     }
+//   }
+
+//   return output;
+// }
+
+// YOUR PSEUDOCODE GOES HERE
+// define empty output string
+// iterate over input string
+// add complement to output string
+// return output
+
 const dnaTransform = (string) => {
+  // YOUR CODE GOES HERE
   let output = '';
 
-  for (const el of string) {
+  const complements = {
+    A: 'T',
+    T: 'A',
+    C: 'G',
+    G: 'C',
+  };
+
+  const split = string.split('');
+
+  for (const el of split) {
+    // console.log('el of string', el);
+    // output.push([el, pairs[el]]);
     if (el === 'A') {
-      output += 'T';
-    } else if (el === 'T') {
-      output += 'A';
-    } else if (el === 'C') {
-      output += 'G';
-    } else if (el === 'G') {
-      output += 'C';
+      output += complements[el];
+    }
+    if (el === 'T') {
+      output += complements[el];
+    }
+    if (el === 'C') {
+      output += complements[el];
+    }
+    if (el === 'G') {
+      output += complements[el];
     }
   }
 
