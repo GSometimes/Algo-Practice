5a1b4f9b77641590135923fe9637be596db424f6
Gore Sometimes <gore.auluck@gmail.com>
2023-07-05 17:13:58 -0700

    deleted extra file and completed maximum-number-of-words-found-in-sentence

---
+++ maximum-number-of-words-found-in-sentences/index.js
@@ -0,0 +1,40 @@
+// Directions
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+// You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+// Return the maximum number of words that appear in a single sentence.
+
+function mostWordsFound(sentences) {
+  // define variable for counter
+  let maxWords = 0;
+
+  // iterate over sentences
+  for (const sentence of sentences) {
+    // split sentence into words
+    const words = sentence.split(' ');
+    // define variable for word count
+    const wordCount = words.length;
+
+    // if word count is greater than max words
+    // reassign max words to word count
+    if (wordCount > maxWords) {
+      maxWords = wordCount;
+    }
+  }
+
+  // return counter
+  return maxWords;
+}
+
+console.log(
+  mostWordsFound([
+    'alice and bob love leetcode',
+    'i think so too',
+    'this is great thanks very much',
+  ])
+);
+console.log(
+  mostWordsFound(['please wait', 'continue to fight', 'continue to win'])
+);
